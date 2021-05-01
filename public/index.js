f3a12527f68fc2180440606631d8e5578fb2998b
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-05-01 13:43:31 +0100

    Reformat and add hive stats

--- public/index.js
+++ public/index.js
@@ -1,40 +1,135 @@
-async function initApp() {
-    const response = await fetch('messages.json')
-    var responseText = await response.text()
-    this.messages = await JSON.parse(responseText)
-    this.idx = lunr(function () {
-        this.ref('name')
-        this.field('text')
-        messages.forEach(function (doc) {
-            this.add(doc)
-        }, this)
-    })
+function fade(element) {
+    var op = 0.1 // initial opacity
+    element.style.display = 'block'
+    var timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer)
+        }
+        element.style.opacity = op
+        element.style.filter = 'alpha(opacity=' + op * 100 + ')'
+        op += op * 0.1
+    }, 10)
+    document.getElementById('loader').style.display = 'none'
 }
-function copyText(pTag) {
-    pTag.select()
-    pTag.setSelectionRange(0, 99999)
-    document.execCommand('copy')
+
+function initLineGraph() {
+    const labels = []
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Hive',
+                backgroundColor: 'rgb(180, 225, 255)',
+                borderColor: 'rgb(180, 225, 255)',
+                data: [],
+            },
+        ],
+    }
+    const config = {
+        type: 'line',
+        data,
+        scales: {},
+    }
+    return new Chart(document.getElementById('hiveChartLine'), config)
 }
-function search() {
-    var idx = this.idx
-	let results
-	try {
-		results = idx.search(document.getElementById('searchBox').value + '~3')
-	} catch(error) {
-		return
-	}
-    var searchResults = document.getElementById('searchResults')
-    searchResults.innerHTML = ''
-    results.forEach(function (value) {
-        var messageFromIndex = this.messages.find(
-            (message) => value.ref === message.name
+function updateLineGraph(chart, playerCount) {
+    let time = new Date(Date.now())
+    chart.data.labels.push(
+        time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+    )
+    chart.data.datasets[0].data.push(playerCount)
+    chart.update()
+}
+function initPieChart() {
+    const data = {
+        labels: ['Mineplex', 'Lifeboat', 'CubeCraft', 'Hive', 'Galaxite'],
+        datasets: [
+            {
+                label: 'My First Dataset',
+                data: [],
+                backgroundColor: [
+                    'rgb(237, 222, 164)',
+                    'rgb(171, 135, 255)',
+                    'rgb(255, 172, 228)',
+                    'rgb(180, 225, 255)',
+                    'rgb(193, 255, 155)',
+                ],
+                hoverOffset: 4,
+            },
+        ],
+    }
+    const config = {
+        type: 'pie',
+        data: data,
+    }
+    return new Chart(document.getElementById('hiveChartPie'), config)
+}
+function updatePieGraph(
+    chart,
+    playerCount1,
+    PlayerCount2,
+    PlayerCount3,
+    PlayerCount4,
+    PlayerCount5
+) {
+    chart.data.datasets[0].data = [
+        playerCount1,
+        PlayerCount2,
+        PlayerCount3,
+        PlayerCount4,
+        PlayerCount5,
+    ]
+    chart.update()
+}
+
+let socket = new WebSocket('wss://bedrock.minetrack.me/')
+socket.onopen = function (e) {
+    console.log('Established connection to minetrack')
+    this.HiveChartLine = initLineGraph()
+    this.ChartPie = initPieChart()
+}
+
+socket.onmessage = function (event) {
+    let data = JSON.parse(event.data)
+    if (!data.updates) return
+    let MineplexPlayerCount = data.updates[0].playerCount
+    let LifeboatPlayerCount = data.updates[1].playerCount
+    let CubePlayerCount = data.updates[3].playerCount
+    let HivePlayerCount = data.updates[4].playerCount
+    let GalaxitePlayerCount = data.updates[6].playerCount
+    document.getElementById('hivePlayerCount').innerHTML = HivePlayerCount
+    if (this.HiveChartLine) {
+        updateLineGraph(this.HiveChartLine, HivePlayerCount)
+    }
+    if (this.ChartPie) {
+        updatePieGraph(
+            this.ChartPie,
+            MineplexPlayerCount,
+            LifeboatPlayerCount,
+            CubePlayerCount,
+            HivePlayerCount,
+            GalaxitePlayerCount
         )
-        var fieldName = 'text-' + messageFromIndex.name
-        searchResults.innerHTML += `
-			<h4>${messageFromIndex.name}</h4>
-			<textArea class="form-control" id="${fieldName}" onclick="copyText(this)" readonly="">${messageFromIndex.text}</textArea><br>
-		`
-        document.getElementById(fieldName).style.height =
-            document.getElementById(fieldName).scrollHeight + 'px'
-    })
+    }
+    if (!this.faded) {
+        fade(document.getElementById('graphs'))
+        this.faded = true
+    }
 }
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log(
+            `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+        )
+    } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('Connection was lost to websocket')
+    }
+}
+
+socket.onerror = function (error) {
+    console.error(error.message)
+}
+function setupGraph() {}
